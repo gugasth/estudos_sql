647bc6250d61570f9afe66e87ebde890dd30faea
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2024-01-24 09:36:44 -0300

    select

---
+++ SQL for Data Science/select.sql
@@ -0,0 +1,14 @@
+-- seleciona uma coluna da tabela
+SELECT coluna_x FROM TABELA
+
+-- seleciona algumas colunas da tabela
+-- forma 1
+SELECT coluna_x, coluna_y FROM TABELA
+
+-- forma 2 (melhor)
+SELECT coluna_x,
+       coluna_y
+FROM Produtos
+
+-- seleciona todas as colunas da tabela
+SELECT * FROM TABELA
