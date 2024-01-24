02029de9e4c9a74cd0634d93a50d8294c1251bca
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2024-01-24 09:45:54 -0300

    limit rows in select

--- SQL for Data Science/select.sql
+++ SQL for Data Science/select.sql
@@ -1,14 +1,31 @@
 -- seleciona uma coluna da tabela
-SELECT coluna_x FROM TABELA
+SELECT coluna_x FROM TABELA;
 
 -- seleciona algumas colunas da tabela
 -- forma 1
-SELECT coluna_x, coluna_y FROM TABELA
+SELECT coluna_x, coluna_y FROM TABELA;
 
 -- forma 2 (melhor)
 SELECT coluna_x,
        coluna_y
-FROM Produtos
+FROM Produtos;
 
 -- seleciona todas as colunas da tabela
-SELECT * FROM TABELA
+SELECT * FROM TABELA;
+
+-- selecionar colunas da tabela, mas limitando o número de linhas
+
+-- sqlite
+SELECT prod_name
+FROM Products
+LIMIT 5;
+
+-- oracle
+SELECT prod_name
+FROM Products
+WHERE ROWNUM <= 5;
+
+-- db2
+SELECT prod_name
+FROM Products
+FETCH FIRST 5 ROWS ONLY;
