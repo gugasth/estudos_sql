b2c76ec3385fad15887a611ffda60baa2a346d9e
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2024-01-26 12:29:22 -0300

    :sparkles: feat: insert_into.sql

---
+++ SQL for Data Science/insert_into.sql
@@ -0,0 +1,20 @@
+INSERT INTO Shoes
+    (Id,
+    Brand,
+    Type,
+    Color,
+    Price,
+    Desc,
+    )
+VALUES
+    ('1234567',
+    'Gucci',
+    'Slippers',
+    'Pink',
+    '695.0',
+    NULL
+    );
+    
+    
+    
+    
